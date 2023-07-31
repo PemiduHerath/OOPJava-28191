b79fe26139eddfed496430513f1211fecaac77c7
PemiduHerath <136629635+PemiduHerath@users.noreply.github.com>
2023-07-31 08:03:09 -0300

    Add files via upload

---
+++ SE101.3-OOPJava-Lab-28191/Lab1_prac01Q02/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Pemidu Herath");
+        System.out.println("Managemnt Information System");
+    }
+}
